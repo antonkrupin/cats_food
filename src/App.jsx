f33fe35281c09efd8037681bff7f7bc0de3191c6
Anton Krupin <krupin_anton@mail.ru>
2023-01-18 20:36:07 +0300

    add import ProductCard from './components/ProductCard';

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import i18n from './asserts/i18';
+
+import ProductCard from './components/ProductCard';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="title">{i18n.t('pageTitle')}</h1>
+const App = () => (
+  <div className="App">
+    <h1 className="title">{i18n.t('pageTitle')}</h1>
+    <div className="ProductCards">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
