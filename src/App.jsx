e5c15274c073061cd3a99c1a721deaa32d40532c
Anton Krupin <krupin_anton@mail.ru>
2023-01-18 18:42:02 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import i18n from './asserts/i18';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1 className="title">{i18n.t('pageTitle')}</h1>
     </div>
   );
 }
