60c3c9044351793a149933a5b8acaa0fad74f36a
antonkrupin <krupin_anton@mail.ru>
2023-01-19 12:03:56 +0300

    change

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,11 +5,26 @@ import { ReactComponent as Border } from '../asserts/img/Border.svg';
 import './ProductCard.css';
 
 const ProductCard = () => (
-  <span className="ProductCard">
+  <div className="ProductCard">
     <Border className="cardBorder hover" />
-    <img src="" alt="" />
-    <h3>test</h3>
-  </span>
+    <div className="ProductCard_text">
+      <h3>Сказочное заморское яство</h3>
+      <h1>Нямушка</h1>
+      <h2>с рыбой</h2>
+      <h4>
+        40 порций
+        <br />
+        2 мыши в подарок
+      </h4>
+      {/* <img src="../asserts/img/cat.png" alt="Довольный кот" /> */}
+    </div>
+    <div className="price">
+      <div>
+        <h1>0,2</h1>
+        <h2>кг</h2>
+      </div>
+    </div>
+  </div>
 );
 
 export default ProductCard;
