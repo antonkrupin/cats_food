0c5dcd8772e9e9b8b4cd06617830ee4a502dc667
antonkrupin <krupin_anton@mail.ru>
2023-01-19 14:42:30 +0300

    add translations

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,5 +1,44 @@
 export default {
   translation: {
     pageTitle: 'Ты сегодня покормил кота?',
+    productCards: {
+      name: 'Нямушка',
+      title: 'Сказочное заморское яство',
+      product1: {
+        ingredient: 'с фуа-гра',
+        servingsPerPack: 10,
+        gift: 1,
+        weight: '0.5',
+        unit: 'кг',
+      },
+      product2: {
+        ingredient: 'с рыбой',
+        servingsPerPack: 40,
+        gift: 2,
+        weight: '2',
+        unit: 'кг',
+      },
+      product3: {
+        ingredient: 'с курой',
+        servingsPerPack: 100,
+        gift: 5,
+        weight: '5',
+        unit: 'кг',
+      },
+    },
+    servings: {
+      counter: {
+        count_one: '{{ count }} порция',
+        count_few: '{{ count }} порции',
+        count_many: '{{ count }} порций',
+      },
+    },
+    mouses: {
+      counter: {
+        count_one: ' мышь в подарок',
+        count_few: '{{ count }} мыши в подарок',
+        count_many: '{{ count }} мышей в подарок',
+      },
+    },
   },
 };
