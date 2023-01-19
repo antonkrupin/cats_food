90aaaefa166df44863d5e07914d552bf5163443f
antonkrupin <krupin_anton@mail.ru>
2023-01-19 14:43:32 +0300

    add render products

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import ProductCard from './components/ProductCard';
 
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <h1 className="title">{i18n.t('pageTitle')}</h1>
-    <div className="ProductCards">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+const App = () => {
+  const test = [1, 2, 3];
+  return (
+    <div className="App">
+      <h1 className="title">{i18n.t('pageTitle')}</h1>
+      <div className="ProductCards">
+        {test.map((elem) => (
+          <ProductCard id={elem} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
