8b3dde50fd2f467cf8b9e106d51a57462da83da0
antonkrupin <krupin_anton@mail.ru>
2023-01-20 10:52:24 +0300

    change

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,22 +9,6 @@ import { ReactComponent as Border } from '../asserts/img/Border.svg';
 import './ProductCard.css';
 
 const ProductCard = (props) => {
-  /* const {
-    disabled, hovered, selected, id,
-  } = props;
-
-  const className = cn('cardBorder', {
-    disabled,
-    hovered,
-    selected,
-  });
-
-  const priceClassName = cn('price', {
-    'price-disabled': disabled,
-    'price-hovered': hovered,
-    'price-selected': selected,
-  }); */
-
   const { id } = props;
 
   const [selected, setSelected] = useState(false);
@@ -38,29 +22,44 @@ const ProductCard = (props) => {
   });
 
   return (
-    <textbox
-      className="ProductCard"
-      onClick={() => setSelected(!selected)}
-    >
-      <Border className={className} />
-      <div className="ProductCard_text">
-        <h3>{i18n.t('productCards.title')}</h3>
-        <h1>{i18n.t('productCards.name')}</h1>
-        <h2>{i18n.t(`productCards.product${id}.ingredient`)}</h2>
-        <h4>
-          {i18n.t('servings.counter.count', { count: i18n.t(`productCards.product${id}.servingsPerPack`) })}
-          <br />
-          {i18n.t('mouses.counter.count', { count: i18n.t(`productCards.product${id}.gift`) })}
-        </h4>
-        {/* <img src="../asserts/img/cat.png" alt="Довольный кот" /> */}
-      </div>
-      <div className={priceClassName}>
-        <div>
-          <h1>{i18n.t(`productCards.product${id}.weight`)}</h1>
-          <h2>{i18n.t(`productCards.product${id}.unit`)}</h2>
+    <div className="wrapper">
+      <textbox
+        className="ProductCard"
+        onClick={() => setSelected(!selected)}
+        onMouseOut={() => setSelected(!selected)}
+      >
+        <Border className={className} />
+        <div className="ProductCard_text">
+          <h3>{i18n.t('productCards.title')}</h3>
+          <h1>{i18n.t('productCards.name')}</h1>
+          <h2>{i18n.t(`productCards.product${id}.ingredient`)}</h2>
+          <h4>
+            {i18n.t('servings.counter.count', { count: i18n.t(`productCards.product${id}.servingsPerPack`) })}
+            <br />
+            {i18n.t('mouses.counter.count', { count: i18n.t(`productCards.product${id}.gift`) })}
+          </h4>
         </div>
+        <div className={priceClassName}>
+          <div>
+            <h1>{i18n.t(`productCards.product${id}.weight`)}</h1>
+            <h2>{i18n.t(`productCards.product${id}.unit`)}</h2>
+          </div>
+        </div>
+      </textbox>
+      <div className="buyText">
+        {!selected && (
+        <h3>
+          {i18n.t(`productCards.product${id}.nonSelected`)}
+          {' '}
+          <textbox onClick={() => setSelected(!selected)}>{i18n.t(`productCards.product${id}.buyText`)}</textbox>
+          .
+        </h3>
+        )}
+        {selected && (
+        <h3>{i18n.t(`productCards.product${id}.selected`)}</h3>
+        )}
       </div>
-    </textbox>
+    </div>
   );
 };
 
